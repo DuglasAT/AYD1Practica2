fb8d1e4ddbb3127e1632495e70d6d742717b3e89
DuglasAT <avilatorresics@gmail.com>
2018-03-15 23:13:11 -0600

    commit 2 - archivo de operaciones basicas

---
+++ basicOperations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int suma(int operando1, int operando2);
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
+
+int suma(int operando1, int operando2)
+{
+	return operando1 + operando2;
+}
